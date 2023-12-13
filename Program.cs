11cf61b58bde41bccc9748dd1071b224578a1d28
José Helder <joseheldersb@gmail.com>
2023-12-12 21:27:54 -0300

    Desafio implementadogit status

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia("1234", "Black100", "987-556-6456", 128);
+nokia.Ligar();
+nokia. InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone iPhone:");
+Smartphone iphone = new Iphone("3453", "Pro Max 13", "1869-863-2345", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("YouTube");
